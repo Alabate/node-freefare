acc104a5e2ac74650b967a4365f6f3fca65d34e9
Tim Stableford <tim.stableford@gmail.com>
2018-02-27 23:31:58 +0000

    Spaces to tabs

--- examples/ntag.js
+++ examples/ntag.js
@@ -10,7 +10,7 @@ const Freefare = require('../index');
 const freefare = new Freefare();
 
 process.on('unhandledRejection', function(error, p) {
-    console.log('Unhandled Rejection at: Promise', p, 'reason:', error);
+	console.log('Unhandled Rejection at: Promise', p, 'reason:', error);
 });
 
 // The 4 byte buffer for the tag password.
@@ -20,85 +20,85 @@ const kPassword = Buffer.from([0x11, 0x22, 0x33, 0x44]);
 const kPack = Buffer.from([0xaa, 0xaa]);
 
 async function tagAdded(device, tag) {
-    if (tag.getType() == 'NTAG_21x') {
-        await tag.open();
-        await tag.getInfo();
-        const subtype = await tag.getSubType();
-        console.log(`${tag.getFriendlyName()} - ${tag.getUID()} - ${subtype}`);
-        try {
-            await tag.write(0x27, Buffer.from([0x01, 0x02, 0x03, 0x04]));
-            console.log('Read data from 0x27', await tag.read(0x27));
-            // If the read suceeds then do set auth.
-            try {
-                await tag.setAuth(kPassword, kPack, 0x00, true);
-                console.log('Tag auth set');
-            } catch (err) {
-                console.log('Unable to set tag auth', err);
-            }
-        } catch (err) {
-            console.log('Unable to read from tag, attempting auth', err);
-            try {
-                // If other commands are run before this then the tag must be re-opened.
-                await tag.close();
-                await tag.open();
-                await tag.authenticate(kPassword, kPack);
-                console.log('Tag authentication success, attempting read');
-                console.log('Read data from 0x27', await tag.read(0x27));
-                
-                console.log('Disabling auth');
-                try {
-                    await tag.disableAuth();
-                } catch (err) {
-                    console.log('Failed to remove authentication');
-                }
-            } catch (err) {
-                console.log('Tag authenticate failed', err);
-            }
-        }
+	if (tag.getType() == 'NTAG_21x') {
+		await tag.open();
+		await tag.getInfo();
+		const subtype = await tag.getSubType();
+		console.log(`${tag.getFriendlyName()} - ${tag.getUID()} - ${subtype}`);
+		try {
+			await tag.write(0x27, Buffer.from([0x01, 0x02, 0x03, 0x04]));
+			console.log('Read data from 0x27', await tag.read(0x27));
+			// If the read suceeds then do set auth.
+			try {
+				await tag.setAuth(kPassword, kPack, 0x00, true);
+				console.log('Tag auth set');
+			} catch (err) {
+				console.log('Unable to set tag auth', err);
+			}
+		} catch (err) {
+			console.log('Unable to read from tag, attempting auth', err);
+			try {
+				// If other commands are run before this then the tag must be re-opened.
+				await tag.close();
+				await tag.open();
+				await tag.authenticate(kPassword, kPack);
+				console.log('Tag authentication success, attempting read');
+				console.log('Read data from 0x27', await tag.read(0x27));
+				
+				console.log('Disabling auth');
+				try {
+					await tag.disableAuth();
+				} catch (err) {
+					console.log('Failed to remove authentication');
+				}
+			} catch (err) {
+				console.log('Tag authenticate failed', err);
+			}
+		}
 
-        await tag.close();
-    } else {
-        console.log(`${tag.getFriendlyName()} - ${tag.getUID()}`);
-    }
-    startPoll(device);
+		await tag.close();
+	} else {
+		console.log(`${tag.getFriendlyName()} - ${tag.getUID()}`);
+	}
+	startPoll(device);
 }
 
 let lastTag = null;
 const kCardTimeout = 5 * 1000;
 async function listTags(device) {
-    try {
-        const tag = await device.poll();
-        if (tag) {
-            if (!lastTag || Date.now() - lastTag.time > kCardTimeout || lastTag.uid != tag.getUID()) {
-                lastTag = {
-                    time: Date.now(),
-                    uid: tag.getUID()
-                };
-                try {
-                    console.time('tagAdded');
-                    await tagAdded(device, tag);
-                    console.timeEnd('tagAdded');
-                } catch (err) {
-                    console.log('tagAdded call failed', err);
-                    startPoll(device);
-                }
-            } else {
-               startPoll(device); 
-            }
-        }
-    } catch (err) {
-        console.log('Failed to list tags', err);
-    }
+	try {
+		const tag = await device.poll();
+		if (tag) {
+			if (!lastTag || Date.now() - lastTag.time > kCardTimeout || lastTag.uid != tag.getUID()) {
+				lastTag = {
+					time: Date.now(),
+					uid: tag.getUID()
+				};
+				try {
+					console.time('tagAdded');
+					await tagAdded(device, tag);
+					console.timeEnd('tagAdded');
+				} catch (err) {
+					console.log('tagAdded call failed', err);
+					startPoll(device);
+				}
+			} else {
+			   startPoll(device); 
+			}
+		}
+	} catch (err) {
+		console.log('Failed to list tags', err);
+	}
 }
 
 function startPoll(device) {
-    setTimeout(function() {
-        listTags(device);
-    }, 0);
+	setTimeout(function() {
+		listTags(device);
+	}, 0);
 }
 
 (async function() {
-    const devices = await freefare.listDevices();
-    await devices[0].open();
-    startPoll(devices[0]);
+	const devices = await freefare.listDevices();
+	await devices[0].open();
+	startPoll(devices[0]);
 })();
